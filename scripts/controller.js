ee03c154dfe4810de8be04bf9d3a15198dfbb260
jeyn99 <jane.repollo@student.passerellesnumeriques.org>
2019-10-09 18:25:16 +0800

    This is ti be checked

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,14 +1,14 @@
 $(document).ready(function (e) {
-  e.preventDefault;
-  var brokweAdd = "wss://test.mosquitto.org:8081/mqtt";
-  client = mqtt.connect(brokweAdd);
-  client.on("connect", function () {
-      $(document).on('click', 'button', function () {
-          var click = $(this).attr("id")
-          $('h2').empty();
-          $('h2').append(click);
-          client.subscribe('jane/fan/status')
-          client.publish('jane/fan/status', "Turned at " + click + " : " +  new Date($.now()))
-      })
-  })
+    e.preventDefault;
+    var brokweAdd = "wss://test.mosquitto.org:8081/mqtt";
+    client = mqtt.connect(brokweAdd);
+    client.on("connect", function () {
+        $(document).on('click', 'button', function () {
+            var click = $(this).attr("id")
+            $('h2').empty();
+            $('h2').append(click);
+            client.subscribe('jane/fan/status')
+            client.publish('jane/fan/status', "Turned at " + click + " : " + new Date($.now()))
+        })
+    })
 })
